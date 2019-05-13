84cd5d4f01886774afe818fdd2e5cf9a5fc19e4e
bissus17@gmail.com <BissusJX>
2019-05-13 01:24:53 -0500

    faltan ultimos ajustes

--- Reportedemolab2.java
+++ Reportedemolab2.java
@@ -28,8 +28,10 @@ Por cada hijo se debe preguntar el gasto semanal que tienen para: pasajes, bar, 
            String salida ="";
         boolean datos = true;
         int contador = 1;
-        
+        int rep = 001;
+        salida = String.format("%s Reporte Gastos Padres de Familia\n\n",salida);
         while(datos){
+            salida = String.format("%sReporte%d\n\n",salida,rep);
             System.out.println("Ingrese nombres y apellidos del padre de familia: ");
             String nom_ap = teclado.nextLine();
             
@@ -52,25 +54,21 @@ Por cada hijo se debe preguntar el gasto semanal que tienen para: pasajes, bar, 
                 double salidas = teclado.nextDouble();
 
                     contador= contador + 1;
-
-                salida = String.format("%s Reporte Gastos Padres de Familia \n\n"
-                        + " Reporte 001 \n\n Nombre Padre de Familia: %s\n Sueldo"
+                salida = String.format("%s Nombre Padre de Familia: %s\n Sueldo"
                         + " Semanal: $%.0f\n Numero de Hijos:%d\n\n Persona\t"
                         + "Pasaje\t\t Bar\t\t Salidas\n\n Hijo%d\t\t %.1f\t\t"
-                        + "%.1f\t\t %.1f\n\n ",salida, nom_ap,sueldo,num_hj,contador,pasajes,bar,salidas );
+                        + "%.1f\t\t %.1f\n\n ",salida, nom_ap,sueldo,num_hj,
+                        contador,pasajes,bar,salidas );
                  }
-                System.out.println("Ingrese 's' si dese seguir y 'n si desea salir'");
+                System.out.println("Ingrese 's' si desea seguir y 'n' si desea salir");
                 String opcion = teclado.next();
                 if (opcion.equals("n")){
                     datos = false; 
                     //System.out.println(salida);
                     System.out.printf(salida);
+                }else{
+                    rep = rep +1 ;
                 }
-
-             
-             //Persona		Pasaje 		Bar		Salidas
-         
-                
     
         }
         
